d17f43ca6e3d40e50671d97755eb7e468a3b2b76
Valentin <valentinvalentinov027@gmail.com>
2022-03-12 16:42:51 +0200

    Created CategoryViewModel

---
+++ Web/Bookworm.Web.ViewModels/Categories/CategoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Bookworm.Web.ViewModels.Categories
+{
+    using Bookworm.Data.Models;
+    using Bookworm.Services.Mapping;
+
+    public class CategoryViewModel : IMapFrom<Category>
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
