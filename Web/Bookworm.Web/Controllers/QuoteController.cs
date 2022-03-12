e025addb4de3dc42a07524668866f43029844b03
Valentin <valentinvalentinov027@gmail.com>
2022-03-12 13:59:14 +0200

    Created QuoteController with Add Action for Authorized users

---
+++ Web/Bookworm.Web/Controllers/QuoteController.cs
@@ -0,0 +1,27 @@
+﻿namespace Bookworm.Web.Controllers
+{
+    using Bookworm.Data.Models;
+    using Bookworm.Services.Data.Contracts;
+    using Bookworm.Web.ViewModels.Quotes;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class QuoteController : Controller
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly IQuotesService quotesService;
+
+        public QuoteController(UserManager<ApplicationUser> userManager, IQuotesService quotesService)
+        {
+            this.userManager = userManager;
+            this.quotesService = quotesService;
+        }
+
+        [Authorize]
+        public IActionResult Add()
+        {
+            return this.View(new QuoteViewModel());
+        }
+    }
+}
