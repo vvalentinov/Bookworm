4b0f088591e059d544f51c2280d2735913478ccb
Valentin <valentinvalentinov027@gmail.com>
2022-03-12 16:47:34 +0200

    Created Category Controller

---
+++ Web/Bookworm.Web/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿namespace Bookworm.Web.Controllers
+{
+    using Bookworm.Services.Data.Contracts;
+    using Bookworm.Web.ViewModels.Categories;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class CategoryController : Controller
+    {
+        private readonly ICategoriesService categoriesService;
+
+        public CategoryController(ICategoriesService categoriesService)
+        {
+            this.categoriesService = categoriesService;
+        }
+
+        public IActionResult All()
+        {
+            return this.View(this.categoriesService.GetAll<CategoryViewModel>());
+        }
+    }
+}
