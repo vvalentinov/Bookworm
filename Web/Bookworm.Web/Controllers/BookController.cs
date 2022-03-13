653c2aba500e6c0b2355a199477016158a881164
Valentin <valentinvalentinov027@gmail.com>
2022-03-13 13:58:53 +0200

    Changed footer css

--- Web/Bookworm.Web/Controllers/BookController.cs
+++ Web/Bookworm.Web/Controllers/BookController.cs
@@ -1,12 +1,25 @@
 ﻿namespace Bookworm.Web.Controllers
 {
+    using Bookworm.Services.Data.Contracts;
+    using Bookworm.Web.ViewModels.Books;
+    using Bookworm.Web.ViewModels.Categories;
     using Microsoft.AspNetCore.Mvc;
 
     public class BookController : Controller
     {
+        private readonly IBooksService booksService;
+
+        public BookController(IBooksService booksService)
+        {
+            this.booksService = booksService;
+        }
+
         public IActionResult Upload()
         {
-            return this.View();
+            return this.View(new UploadBookFormModel()
+            {
+                Categories = this.booksService.GetBookCategories<CategoryViewModel>(),
+            });
         }
     }
 }
