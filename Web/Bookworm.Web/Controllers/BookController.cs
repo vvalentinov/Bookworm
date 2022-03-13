7da06dbc7c6c4b3b7eb92b6c209a45f9f3287d9a
Valentin <valentinvalentinov027@gmail.com>
2022-03-13 12:52:49 +0200

    Created BookController with Upload Action

---
+++ Web/Bookworm.Web/Controllers/BookController.cs
@@ -0,0 +1,12 @@
+﻿namespace Bookworm.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class BookController : Controller
+    {
+        public IActionResult Upload()
+        {
+            return this.View();
+        }
+    }
+}
