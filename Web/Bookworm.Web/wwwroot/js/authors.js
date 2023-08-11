f0a52ae2e37faf6b22340e35cd37f93915d091a9
Valentin <valentinvalentinov027@gmail.com>
2023-08-11 21:10:36 +0300

    Make change to authors.js

--- Web/Bookworm.Web/wwwroot/js/authors.js
+++ Web/Bookworm.Web/wwwroot/js/authors.js
@@ -16,7 +16,7 @@
     buttonElement.className = "btn btn-outline-danger";
     buttonElement.type = "button";
     buttonElement.id = "deleteAuthorBtn";
-    buttonElement.textContent = "Delete";
+    buttonElement.textContent = "Remove";
     buttonElement.addEventListener('click', function handleClick(event) {
         event.target.parentElement.remove();
         if (authorsCount < 5) {
@@ -31,26 +31,35 @@
     if (authorsCount == 4) {
         addAuthorBtn.disabled = true;
 
-        // Create and append alert message
-        let alertMessageContainer = document.getElementById('alert-message');
-        let alertMessage = document.createElement('div');
-
-        alertMessage.className = 'alert alert-info alert-dismissible fade show animate__animated animate__pulse';
-        alertMessage.role = 'alert';
-        alertMessage.textContent = 'You can only add 5 authors!';
-
-        let alertMessageButton = document.createElement('button');
-        alertMessageButton.type = 'button';
-        alertMessageButton.className = 'btn-close';
-        alertMessageButton.setAttribute('data-bs-dismiss', 'alert');
-        alertMessageButton.setAttribute('aria-label', 'Close');
-
-        alertMessage.appendChild(alertMessageButton);
-        alertMessageContainer.appendChild(alertMessage);
-
-        setTimeout(() => {
-            alertMessage.style.display = 'none';
-        }, 5000);
+        let alertEl = document.getElementsByClassName('alert')[0];
+        if (alertEl == undefined) {
+            alert();
+        }
+        
         return;
     }
+}
+
+
+// Create and append alert message
+function alert() {
+    let alertMessageContainer = document.getElementById('alert-message');
+    let alertMessage = document.createElement('div');
+
+    alertMessage.className = 'alert alert-info alert-dismissible fade show animate__animated animate__pulse';
+    alertMessage.role = 'alert';
+    alertMessage.textContent = 'You can only add 5 authors!';
+
+    let alertMessageButton = document.createElement('button');
+    alertMessageButton.type = 'button';
+    alertMessageButton.className = 'btn-close';
+    alertMessageButton.setAttribute('data-bs-dismiss', 'alert');
+    alertMessageButton.setAttribute('aria-label', 'Close');
+
+    alertMessage.appendChild(alertMessageButton);
+    alertMessageContainer.appendChild(alertMessage);
+
+    setTimeout(() => {
+        alertMessage.remove();
+    }, 5000);
 }
