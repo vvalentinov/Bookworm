9fa69fc66178ea628b2beb4d27170d08416ea5f3
Valentin <valentinvalentinov027@gmail.com>
2024-01-24 07:05:10 +0200

    Create ApiCommentController with response model

---
+++ Web/Bookworm.Web.ViewModels/Comments/SortedCommentsResponseModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Bookworm.Web.ViewModels.Comments
+{
+    using System.Collections.Generic;
+
+    public class SortedCommentsResponseModel
+    {
+        public IEnumerable<CommentViewModel> Comments { get; set; }
+
+        public bool IsUserSignedIn { get; set; }
+
+        public bool IsUserAdmin { get; set; }
+    }
+}
---
+++ Web/Bookworm.Web/Controllers/Api/ApiCommentController.cs
@@ -0,0 +1,33 @@
+﻿namespace Bookworm.Web.Controllers.Api
+{
+    using System.Threading.Tasks;
+
+    using Bookworm.Data.Models;
+    using Bookworm.Services.Data.Contracts;
+    using Bookworm.Web.ViewModels.Comments;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class ApiCommentController : ControllerBase
+    {
+        private readonly ICommentsService commentsService;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public ApiCommentController(ICommentsService commentsService, UserManager<ApplicationUser> userManager)
+        {
+            this.commentsService = commentsService;
+            this.userManager = userManager;
+        }
+
+        [HttpGet(nameof(this.GetSortedByDateAsc))]
+        public async Task<SortedCommentsResponseModel> GetSortedByDateAsc()
+        {
+            ApplicationUser user = await this.userManager.GetUserAsync(this.User);
+            SortedCommentsResponseModel responseModel = await this.commentsService.GetSortedCommentsByDateAscAsync(user);
+
+            return responseModel;
+        }
+    }
+}
