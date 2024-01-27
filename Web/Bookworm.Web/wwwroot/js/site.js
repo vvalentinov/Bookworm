f2a2710fe5887f5d44400627733fa497e8521c09
Valentin <valentinvalentinov027@gmail.com>
2024-01-27 11:23:38 +0200

    Add go to top arrow button

--- Web/Bookworm.Web/wwwroot/js/site.js
+++ Web/Bookworm.Web/wwwroot/js/site.js
@@ -1,1 +1,7 @@
-﻿
+﻿const toTop = document.querySelector('.topArrow');
+
+toTop.addEventListener('click', () => {
+    window.scrollTo(0, 0);
+});
+
+window.addEventListener("scroll", () => toTop.style.display = window.scrollY > 500 ? 'block' : 'none');
