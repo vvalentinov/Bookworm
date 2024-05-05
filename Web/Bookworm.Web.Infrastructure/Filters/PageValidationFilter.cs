be7b4ccad3436c816946820bf2337242a2a8dbfe
Valentin <valentinvalentinov027@gmail.com>
2024-05-05 21:08:08 +0300

    Create PageValidationFilter.cs

---
+++ Web/Bookworm.Web.Infrastructure/Filters/PageValidationFilter.cs
@@ -0,0 +1,23 @@
+﻿namespace Bookworm.Web.Infrastructure.Filters
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Filters;
+
+    public class PageValidationFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var controller = (Controller)context.Controller;
+
+            var isPresent = context.ActionArguments.TryGetValue("page", out var page);
+
+            if (isPresent && (int)page <= 0)
+            {
+                var actionName = (string)context.RouteData.Values["action"];
+                context.Result = controller.RedirectToAction(actionName, new { page = 1 });
+            }
+
+            base.OnActionExecuting(context);
+        }
+    }
+}
