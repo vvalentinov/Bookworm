76453381997aa2f12c939762f85b83dcc1ef3be4
Valentin <valentinvalentinov027@gmail.com>
2024-05-05 21:08:24 +0300

    Create QuoteValidationFilter.cs

---
+++ Web/Bookworm.Web.Infrastructure/Filters/QuoteValidationFilter.cs
@@ -0,0 +1,45 @@
+﻿namespace Bookworm.Web.Infrastructure.Filters
+{
+    using System.Linq;
+
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Filters;
+
+    using static Bookworm.Common.Constants.TempDataMessageConstant;
+
+    public class QuoteValidationFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var controller = (Controller)context.Controller;
+            var action = (string)context.RouteData.Values["action"];
+            var model = context.ActionArguments["model"];
+
+            if (action == "Edit")
+            {
+                var id = model.GetType().GetProperty("Id").GetValue(model);
+
+                if ((int)id <= 0)
+                {
+                    controller.TempData[ErrorMessage] = "The quote id must be a positive number!";
+                    context.Result = controller.RedirectToAction("UserQuotes");
+                }
+            }
+
+            if (context.ModelState.IsValid == false)
+            {
+                controller.TempData[ErrorMessage] = string.Join(
+                    ", ",
+                    context
+                    .ModelState
+                    .Values
+                    .SelectMany(v => v.Errors)
+                    .Select(x => x.ErrorMessage));
+
+                context.Result = controller.View(model);
+            }
+
+            base.OnActionExecuting(context);
+        }
+    }
+}
