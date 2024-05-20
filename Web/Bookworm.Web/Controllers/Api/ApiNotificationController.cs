2c6fb11bcf096395933aff2c2eaf8bb294c293ed
Valentin <valentinvalentinov027@gmail.com>
2024-05-20 15:36:20 +0300

    Create notification controllers

---
+++ Web/Bookworm.Web/Controllers/Api/ApiNotificationController.cs
@@ -0,0 +1,45 @@
+﻿namespace Bookworm.Web.Controllers.Api
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using Bookworm.Data.Models;
+    using Bookworm.Services.Data.Contracts;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ApiNotificationController : ApiBaseController
+    {
+        private readonly INotificationService notificationService;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public ApiNotificationController(
+            INotificationService notificationService,
+            UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+            this.notificationService = notificationService;
+        }
+
+        [HttpGet(nameof(GetUserNotificationsCount))]
+        public async Task<int> GetUserNotificationsCount()
+        {
+            var userId = this.userManager.GetUserId(this.User);
+            return await this.notificationService.GetUserNotificationsCountAsync(userId);
+        }
+
+        [HttpDelete(nameof(DeleteNotification))]
+        public async Task<ActionResult<int>> DeleteNotification(int id)
+        {
+            try
+            {
+                var userId = this.userManager.GetUserId(this.User);
+                return await this.notificationService.DeleteUserNotificationAsync(userId, id);
+            }
+            catch (Exception ex)
+            {
+                return this.BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ Web/Bookworm.Web/Controllers/NotificationController.cs
@@ -0,0 +1,32 @@
+﻿namespace Bookworm.Web.Controllers
+{
+    using System.Threading.Tasks;
+
+    using Bookworm.Data.Models;
+    using Bookworm.Services.Data.Contracts;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("Notifications")]
+    public class NotificationController : BaseController
+    {
+        private readonly INotificationService notificationService;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public NotificationController(
+            INotificationService notificationService,
+            UserManager<ApplicationUser> userManager)
+        {
+            this.notificationService = notificationService;
+            this.userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var userId = this.userManager.GetUserId(this.User);
+            var model = await this.notificationService.GetUserNotificationsAsync(userId);
+            return this.View(model);
+        }
+    }
+}
