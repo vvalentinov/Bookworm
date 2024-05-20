3d805851955e53b4bb1fd227417164240ca9e0be
Valentin <valentinvalentinov027@gmail.com>
2024-05-20 15:37:39 +0300

    Create notification view models

---
+++ Web/Bookworm.Web.ViewModels/Notification/NotificationListViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Bookworm.Web.ViewModels.Notification
+{
+    using System.Collections.Generic;
+
+    public class NotificationListViewModel
+    {
+        public IEnumerable<NotificationViewModel> Notifications { get; set; }
+    }
+}
---
+++ Web/Bookworm.Web.ViewModels/Notification/NotificationViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Bookworm.Web.ViewModels.Notification
+{
+    using Bookworm.Data.Models;
+    using Bookworm.Services.Mapping;
+
+    public class NotificationViewModel : IMapFrom<Notification>
+    {
+        public int Id { get; set; }
+
+        public string Content { get; set; }
+    }
+}
