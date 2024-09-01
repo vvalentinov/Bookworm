8f13318f49dfae6332b38f5d34f8e100b361e419
Valentin <valentinvalentinov027@gmail.com>
2024-09-01 14:02:41 +0300

    Create operation result object
    Create operation result object to return from service layer and create claims principal extensions

---
+++ src/Bookworm.Common/OperationResult.cs
@@ -0,0 +1,48 @@
+﻿namespace Bookworm.Common
+{
+    public class OperationResult
+    {
+        private protected OperationResult(
+            bool isSuccess,
+            string successMessage = null,
+            string errorMessage = null)
+        {
+            this.IsSuccess = isSuccess;
+            this.SuccessMessage = successMessage;
+            this.ErrorMessage = errorMessage;
+        }
+
+        public bool IsSuccess { get; }
+
+        public string SuccessMessage { get; }
+
+        public string ErrorMessage { get; }
+
+        public static OperationResult Ok() => new(true);
+
+        public static OperationResult Ok(string successMessage) => new(true, successMessage);
+
+        public static OperationResult<T> Ok<T>(T data) => new(true, data);
+
+        public static OperationResult<T> Ok<T>(T data, string successMessage) => new(true, data, successMessage);
+
+        public static OperationResult Fail(string errorMessage) => new(false, null, errorMessage);
+
+        public static OperationResult<T> Fail<T>(string errorMessage) => new(false, default, null, errorMessage);
+    }
+
+    public class OperationResult<T> : OperationResult
+    {
+        public OperationResult(
+            bool isSuccess,
+            T data,
+            string successMessage = null,
+            string errorMessage = null)
+            : base(isSuccess, successMessage, errorMessage)
+        {
+            this.Data = data;
+        }
+
+        public T Data { get; }
+    }
+}
---
+++ src/Web/Bookworm.Web/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Bookworm.Web.Extensions
+{
+    using System.Security.Claims;
+
+    using static System.Security.Claims.ClaimTypes;
+
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string GetId(this ClaimsPrincipal user)
+            => user.FindFirstValue(NameIdentifier);
+    }
+}
