67cd38ce15ce70f3f00f3f761f5b39d470e5f14b
Valentin <valentinvalentinov027@gmail.com>
2024-09-10 14:06:51 +0300

    Create animate js util file

---
+++ src/Web/Bookworm.Web/wwwroot/js/animate.js
@@ -0,0 +1,48 @@
+﻿export function animateCSS(
+    element,
+    animation,
+    speed,
+    shouldBeRemoved = false,
+    prefix = 'animate__') {
+
+    new Promise((resolve, reject) => {
+
+        const animationName = `${prefix}${animation}`;
+
+        const animationSpeed = `${prefix}${speed}`;
+
+        element.classList.add(
+            `${prefix}animated`,
+            animationName,
+            animationSpeed);
+
+        if (shouldBeRemoved) {
+            function handleAnimationEnd(event) {
+                event.stopPropagation();
+                element.classList.remove(`${prefix}animated`, animationName, animationSpeed);
+                resolve('Animation ended');
+            }
+
+            element.addEventListener(
+                'animationend',
+                handleAnimationEnd,
+                { once: true });
+        }
+    });
+}
+
+export function animateCssRemoveAnimation(
+    element,
+    animationName,
+    animationSpeed) {
+
+    const prefix = 'animate__';
+
+    const animation = `${prefix}${animationName}`;
+    const speed = `${prefix}${animationSpeed}`;
+
+    element.classList.remove(
+        `${prefix}animated`,
+        animation,
+        speed);
+}
--- src/Web/Bookworm.Web/wwwroot/js/site.js
+++ src/Web/Bookworm.Web/wwwroot/js/site.js
@@ -1,3 +1,37 @@
-﻿const toTop = document.querySelector('.topArrow');
+﻿import { animateCSS, animateCssRemoveAnimation } from './animate.js';
+
+const toTop = document.querySelector('.topArrow');
+
 toTop.addEventListener('click', () => window.scrollTo(0, 0));
-window.addEventListener("scroll", () => toTop.style.display = window.scrollY > 500 ? 'block' : 'none');
+
+toTop.addEventListener('animationend', (e) => {
+    const button = e.currentTarget;
+
+    if (button.classList.contains('animate__zoomOut')) {
+
+        animateCssRemoveAnimation(toTop, 'zoomOut', 'faster');
+
+        button.style.display = 'none';
+    }
+});
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 500) {
+
+        if (toTop.classList.contains('animate__zoomOut')) {
+            animateCssRemoveAnimation(toTop, 'zoomOut', 'faster');
+        }
+
+        animateCSS(toTop, 'zoomIn', 'faster');
+
+        toTop.style.display = 'block';
+
+    } else {
+
+        if (toTop.classList.contains('animate__zoomIn')) {
+            animateCssRemoveAnimation(toTop, 'zoomIn', 'faster');
+        }
+
+        animateCSS(toTop, 'zoomOut', 'faster');
+    }
+});
